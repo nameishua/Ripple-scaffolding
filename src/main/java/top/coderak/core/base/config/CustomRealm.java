fb134d4837cf4fc88aa88af9d459976d3e8abfdd
tshakh <tshakh@126.com>
2026-06-14 00:37:44 +0800

    refactor: upgrade dependencies and fix security vulnerabilities

--- src/main/java/top/coderak/core/base/config/CustomRealm.java
+++ src/main/java/top/coderak/core/base/config/CustomRealm.java
@@ -1,5 +1,6 @@
 package top.coderak.core.base.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
